20fb9bf332b4550d02f2febc5da14e467e016755
Karolis Ramanauskas <hello@karolisram.com>
2017-12-22 12:18:29 +0200

    Fix component code so it renders again.

--- frontend/src/components/Redeem.js
+++ frontend/src/components/Redeem.js
@@ -1,3 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Form, Input, Button } from 'antd';
+const FormItem = Form.Item;
+
+
+class Redeem extends Component {
+	constructor(props) {
+		super(props);
 
 		this.state = {
 			amount: '',
