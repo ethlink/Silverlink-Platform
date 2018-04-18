aa83af5e3ff3b2c21f8528737ccf6ef7e9f481b1
Link Platform <steven@ethereum.link>
2018-04-18 13:05:53 -0500

    Update Instructions.js

--- frontend/src/components/Instructions.js
+++ frontend/src/components/Instructions.js
@@ -18,14 +18,13 @@ const Instructions = props => (
     ]}
     visible={props.visible}
   >
-    <p>In order to use this application you need a Metamask account and being connected to Rinkeby Test Network instead of Main net.</p>
+    <p>In order to use this application you need <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">Metamask</a> Extension.</p>
 
     <h3>How to Use Metamask:</h3>
 
     <p>1. Download and install <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">Metamask</a> extension.</p>
-    <p>2. Connect to Rinkeby Test Net by clicking the top right arrow and selecting Rinkeby Test Network.</p>
 
-    <p>3. Now that you are connected into Rinkeby Test Network you need some Rinkeby Ether. Fortunately, you can get it for free just by completing a social network <a href="https://faucet.rinkeby.io/" target="_blank" rel="noopener noreferrer">challenge</a>. Now you are ready to use Link Platform.</p>
+    <p>2. Now that you are connected into Rinkeby Test Network you need some Rinkeby Ether. Fortunately, you can get it for free just by completing a social network <a href="https://faucet.rinkeby.io/" target="_blank" rel="noopener noreferrer">challenge</a>. Now you are ready to use Link Platform.</p>
 
     <h3>Performing an action:</h3>
 
